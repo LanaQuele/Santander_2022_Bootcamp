ad4772803f0987c672eddbe9492eef9b43ad0a30
Lana <lanaquele@gmail.com>
2022-08-11 09:22:07 -0300

    correção de comentário

--- Desafio_Ecommerce_Angular/src/app/components/bookstore-app/product-list/product-list.component.ts
+++ Desafio_Ecommerce_Angular/src/app/components/bookstore-app/product-list/product-list.component.ts
@@ -20,13 +20,13 @@ export class ProductListComponent implements OnInit {
   ngOnInit(): void {
 
     this.booksService.getBook().subscribe(data => {
-      this.livros = this.formataPrecos(data.books.splice(0, 5)); //Só os 5 primeiros livros, para não encher muito a página.
-      console.log(this.livros);
+      this.livros = this.formataPrecos(data.books.splice(0, 5)); 
+        console.log(this.livros);
     })
 
   }
 
-  //A API retorna o preço dos livros como uma string com o símbolo do dólar na frente, então tive de criar uma função para remover ele antes de poder utilizar o pipe de currency. Aproveitei para transformar logo o preço em number.
+  
   formataPrecos(lista: Array<book>) {
     for (let i = 0; i < lista.length; i++) {
       const precoSemSimbolo = lista[i].price.substring(1);
